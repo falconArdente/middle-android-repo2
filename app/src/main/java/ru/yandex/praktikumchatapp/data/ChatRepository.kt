c8e08e7c7f737b94c4d0527eec498449fa0398af
falconArdente <iwsokolov@ya.ru>
2025-10-31 14:22:58 +0300

    Повторный запрос и тесты

--- app/src/main/java/ru/yandex/praktikumchatapp/data/ChatRepository.kt
+++ app/src/main/java/ru/yandex/praktikumchatapp/data/ChatRepository.kt
@@ -1,12 +1,19 @@
 package ru.yandex.praktikumchatapp.data
 
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.retryWhen
+
 
 class ChatRepository(
     private val api: ChatApi = ChatApi()
 ) {
-
+    private val retryInitialDelay = 60L
     fun getReplyMessage(): Flow<String> {
-        return api.getReply()  // TODO Задание 2: добавьте обработку ошибок
+        return api.getReply()
+            .retryWhen { cause, attempt ->
+                delay(retryInitialDelay * attempt)
+                true
+            }// Задание 2: добавьте обработку ошибок Done
     }
 }
